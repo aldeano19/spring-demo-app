45230b68aa01ef0869e5a7770d43ca122d1e905a
dibu2018 <erieljr1@gmail.com>
2018-08-14 04:27:04 -0400

    added Swagger controller documentation

--- src/main/java/com/eriel/amex/demo/controller/UserController.java
+++ src/main/java/com/eriel/amex/demo/controller/UserController.java
@@ -7,6 +7,10 @@ import com.eriel.amex.demo.helper.LoggingHelper;
 import com.eriel.amex.demo.model.User;
 import com.eriel.amex.demo.service.FileUploadService;
 import com.eriel.amex.demo.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +23,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Api(value = "usersaccess",description = "Operations to interact with users")
 public class UserController {
 
     private LoggingHelper loggingHelper = LoggingHelper.getInstance();
@@ -36,6 +41,12 @@ public class UserController {
      * @return Some info about this user.
      */
     @RequestMapping(value = "/info/{userAccountNumber}", method = RequestMethod.GET)
+    @ApiOperation(value = "Get some info about the user with this account", response = MinimalUserInfoDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved User info"),
+            @ApiResponse(code = 404, message = "Account not fount"),
+            @ApiResponse(code = 422, message = "Bad User lookup info")
+    })
     public MinimalUserInfoDto getMinimalUserInfo(@PathVariable String userAccountNumber) throws AccountDoesNotExistException {
         String message = String.format("Getting User by account number = %s", userAccountNumber);
         loggingHelper.logLine(message);
@@ -48,6 +59,7 @@ public class UserController {
      * @return The successfully created User in the database.
      */
     @RequestMapping(method = RequestMethod.POST)
+    @ApiOperation(value = "Create a new User", response = User.class)
     public User create(CreateUserDto createUserDto){
         String message = String.format("Creating new user = %s", createUserDto);
         loggingHelper.logLine(message);
@@ -56,6 +68,7 @@ public class UserController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/usersUpload")
+    @ApiOperation(value = "Upload a zip file with spreadsheets where every row is a user")
     public void bulkUpload(@RequestParam("file") MultipartFile multipartFile){
         String message = "Processing file " + multipartFile.getOriginalFilename();
         loggingHelper.logLine(message);
@@ -64,6 +77,7 @@ public class UserController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/all")
+    @ApiOperation(value = "Get all existing users. No pagination implemented", response = List.class)
     public List<User> findAll(){
         return userService.getAll();
     }
